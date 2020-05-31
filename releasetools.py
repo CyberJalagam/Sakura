f6d5d53f040492b8d05ad39109e15a7f0531c8b2
Ansh Singh <anshsingh.14.lko@gmail.com>
2020-05-31 18:03:57 +0530

    CPH1859: Update releasetools.py

--- releasetools.py
+++ releasetools.py
@@ -21,9 +21,3 @@ def FullOTA_InstallEnd(info):
     info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/platform/bootdevice/by-name/system", "/system_root");')
     info.script.AppendExtra('run_program("/sbin/sed", "-i", "/fuseblk/d", "/system_root/system/etc/selinux/plat_sepolicy.cil");')
     info.script.AppendExtra('unmount("/system_root");')
-
-def AddImage(info, basename, dest):
-  name = basename
-  data = info.input_zip.read("IMAGES/" + basename)
-  common.ZipWriteStr(info.output_zip, name, data)
-  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
