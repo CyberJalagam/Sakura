0143f2ba0e95cf46be6d0e8ed92345122113610b
buddi56 <ynaveenk.0513@gmail.com>
2020-06-01 12:54:54 +0000

    CPH1859: patches: updated

--- patches/uninstall.sh
+++ patches/uninstall.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="frameworks/base"
+dirs="external/skia frameworks/base frameworks/opt/net/wifi system/netd"
 
 for dir in $dirs ; do
 	cd $rootdirectory
