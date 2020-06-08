57636b5d2fab2432e0353f02b4863b3a5a68d883
Ansh Singh <anshsingh.14.lko@gmail.com>
2020-06-08 10:43:01 +0530

    CPH1859: Update BoardConfig.mk
    define releasetools path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,6 +113,9 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
+
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
